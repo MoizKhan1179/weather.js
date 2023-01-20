3e24f50284bc34a45c5b1a679e6e370041233e13
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2023-01-20 18:13:07 +0500

    Add files via upload

--- weather.js
+++ weather.js
@@ -17,12 +17,15 @@ function weatherInfo(){
   document.querySelector('#location').innerHTML=`${response.data.location.region}`
   document.querySelector('#currentTemp').innerHTML=`${response.data.current.temp_c}°C`
   document.querySelector('#tempF').innerHTML=`${response.data.current.temp_f}°F`
-  document.querySelector('#humidity').innerHTML=`${response.data.current.humidity}`
+  document.querySelector('#humidity').innerHTML=`Hum :${response.data.current.humidity}`
   document.querySelector('#winDir').innerHTML=`Dir : ${response.data.current.wind_dir}`
-  document.querySelector('#high').innerHTML=`H  ${response.data.current.pressure_in}°C`
-  document.querySelector('#low').innerHTML=`L ${response.data.current.pressure_mb}°C`
+  document.querySelector('#high').innerHTML=`H : ${response.data.current.pressure_in}°C`
+  document.querySelector('#low').innerHTML=`L : ${response.data.current.pressure_mb}°C`
   document.querySelector('#img').src = `https:${response.data.current.condition.icon}`
   document.querySelector('#condition').innerHTML=`${response.data.current.condition.text}`
+  document.querySelector('#pressure').innerHTML=`${response.data.current.pressure_in}atm`
+  document.querySelector('#prec').innerHTML=` ${response.data.current.precip_mm}mm`
+  document.querySelector('#windKph').innerHTML=`${response.data.current.wind_kph}Kph`
 })
 
 .catch(function (error) {
